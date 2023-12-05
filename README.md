a47d70ae7611ee4201aaed27073df3087a14c3a3
Graziano Dinocca <grazianodinocca@icloud.com>
2023-12-05 18:04:24 +0100

    added free-hand drawing with undo&redo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Screen Crab Annotation Tools
+
+Annotation Tools for Screen Crab in pure Rust
+
+## Development
+Test the application using the following commands:
+- `cargo run -- --path ./test.png` for high-resolution fullscreen image
+- `cargo run -- --path ./test-mini.png` for tiny image
--- src/painter.rs
+++ src/painter.rs
@@ -1,10 +1,11 @@
-use crate::utils::{AppState};
-use druid::{Point, Rect, Widget};
+use crate::utils::{AppState, Action};
+use druid::{Rect, Widget};
 use druid::RenderContext;
 use druid::{Env, Color};
 use druid::kurbo::{Circle, Line};
-use druid::piet::{ImageFormat, InterpolationMode, StrokeStyle, Text};
+use druid::piet::{ImageFormat, InterpolationMode};
 use druid::widget::prelude::*;
+use druid::piet::Image;
 
 pub struct DrawingWidget;
 
@@ -12,24 +13,43 @@ impl Widget<AppState> for DrawingWidget {
     fn event(&mut self, ctx: &mut druid::EventCtx, event: &druid::Event, data: &mut AppState, _env: &Env) {
         // Handle user input events for drawing here
         match event {
+            Event::Paste(clipboard) => {
+                println!("Pasted!");
+            }
             Event::Zoom(value) => {
                 println!("Zoomed! {}", value);
             }
             Event::MouseDown(e) => {
                 data.is_drawing = true;
-                data.actions.push(Vec::new());
-                println!("{}", data.is_drawing);
+                let mut action = Action::new(&data.selection);
+                match action {
+                    Action::Pencil(ref mut points) => { points.push(Circle::new(e.pos, 1.5)); }
+                    Action::Pen(ref mut points) => { points.push(e.pos); }
+                    Action::Highlighter(ref mut points) => { points.push(e.pos); }
+                    Action::Eraser(ref mut points) => { /* TODO */ }
+                    Action::Text(ref mut points) => { /* TODO */ }
+                    _ => {}
+                }
+                data.actions.push(action);
+                ctx.request_paint();
             }
             Event::MouseMove(e) => {
                 if data.is_drawing {
-                    data.actions.last_mut().unwrap().push(e.pos);
-                    println!("{:?}", data.actions);
+                    if let Some(action) = data.actions.last_mut() {
+                        match action {
+                            Action::Pencil(points) => { points.push(Circle::new(e.pos, 1.5)); }
+                            Action::Pen(points) => { points.push(e.pos); }
+                            Action::Highlighter(points) => { points.push(e.pos); }
+                            Action::Eraser(points) => { /* TODO */ }
+                            Action::Text(points) => { /* TODO */ }
+                            _ => {}
+                        }
+                    }
                     ctx.request_paint();
                 }
             }
             Event::MouseUp(e) => {
                 data.is_drawing = false;
-                println!("{}", data.is_drawing);
             }
             _ => (),
         }
@@ -52,13 +72,36 @@ impl Widget<AppState> for DrawingWidget {
         let image = ctx.make_image(data.image.width(), data.image.height(), data.image.raw_pixels(), ImageFormat::RgbaSeparate).unwrap();
         ctx.draw_image(&image, Rect::new(0f64, 0f64, width, height), InterpolationMode::Bilinear);
         for action in &data.actions {
-            for pair in action.windows(2) {
-                if let [start, end] = pair {
-                    let line = Line::new(*start, *end);
-                    ctx.stroke(line, &druid::Color::BLACK, 2.0);
+            match action {
+                Action::Pencil(action) => {
+                    action.iter().for_each(|circle| ctx.fill(circle, &Color::BLACK));
                 }
+                Action::Highlighter(action) => {
+                    if action.len() < 2 {
+                        ctx.fill(Circle::new(*action.last().unwrap(), 5.0), &druid::Color::BLACK.with_alpha(0.25));
+                    }
+                    for pair in action.windows(2) {
+                        if let [start, end] = pair {
+                            let line = Line::new(*start, *end);
+                            ctx.stroke(line, &druid::Color::BLACK.with_alpha(0.25), 10.0);
+                        }
+                    }
+                }
+                Action::Pen(action) => {
+                    if action.len() < 2 {
+                        ctx.fill(Circle::new(*action.last().unwrap(), 1.0), &druid::Color::BLACK);
+                    }
+                    for pair in action.windows(2) {
+                        if let [start, end] = pair {
+                            let line = Line::new(*start, *end);
+                            ctx.stroke(line, &druid::Color::BLACK, 2.0);
+                        }
+                    }
+                }
+                Action::Eraser(action) => { /* TODO */ }
+                Action::Text(action) => { /* TODO */ }
+                _ => {}
             }
         }
-
     }
 }
